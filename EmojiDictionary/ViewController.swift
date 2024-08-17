31ed0bc3c186fe02beac6d9fa442e0ccbeab0857
tanishksahni <tanishk.sahni2002@gmail.com>
2024-08-17 10:24:01 +0530

    Initial Commit

---
+++ EmojiDictionary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EmojiDictionary
+//
+//  Created by Tanishk Sahni on 17/08/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
